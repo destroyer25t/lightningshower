936c09b6e82be16dcb60ebee3d38ee63e595ee41
Oleg <js7ussr@gmail.com>
2016-04-03 21:15:31 +0500

    *Отключены логи и счет времени работы цикла и поиска контуров *Изменена логика функции preparingBeforeFindContours - теперь она boolean и возвращает true если молния на кадре была найдена

--- app/src/main/java/com/example/dogan/ligntningshower/OpenCVHandler.java
+++ app/src/main/java/com/example/dogan/ligntningshower/OpenCVHandler.java
@@ -30,63 +30,63 @@ public class OpenCVHandler {
         CvSeq contours = new CvSeq();
         CvRect rect = null;
         //находим контуры
-        long startTime = System.currentTimeMillis();    //засекаем время получения кадро
+        //long startTime = System.currentTimeMillis();    //засекаем время получения кадро
         int contoursCont = cvFindContours(Iat, storage, contours, sizeof(CvContour.class), CV_RETR_LIST, CV_LINK_RUNS, cvPoint(0, 0));
-        long endTime = System.currentTimeMillis();
-        Log.d("TRULALA", "Время работы cvFindContours: " + ((endTime - startTime) / 1000f));
-       // Log.d("TRULALA", Integer.toString(contoursCont));
-        //нарисуем контуры
-        boolean isdetected = false;
+        //long endTime = System.currentTimeMillis();
+        //Log.d("TRULALA", "Время работы cvFindContours: " + ((endTime - startTime) / 1000f));
+        // Log.d("TRULALA", Integer.toString(contoursCont));
         int counter = 0;
-        startTime = System.currentTimeMillis();
+        //startTime = System.currentTimeMillis();
         if (contoursCont >= 0) {
             for (CvSeq seq0 = contours; seq0 != null; seq0 = seq0.h_next()) {
                 double area = Math.abs(cvContourArea(seq0));    //площадь контура
                 double perim = cvContourPerimeter(seq0);    //периметр контура
                 double compact = area / (perim * perim);    //какая то придуманная формула для расчета компактности
-                if (compact < 0.01 && area > areaImage && !isdetected) { //area>15
-                    double halfper = perim / 1.5;
+                if (compact < 0.01 && area > areaImage) { //area>15
                     rect = cvBoundingRect(seq0, 0);
-                    if ((rect.height() * 3 > Igray.height()) || (halfper > Igray.height()) || (rect.height() > rect.width() * 3))
-                        return true;//isdetected = true;
+                    if ((rect.height() * 3 > Igray.height()) || (rect.height() > rect.width() * 3))
+                        return true;
                 }
             }
 
         }//Log.d("TRULALA", "Молния найдена");
-        endTime = System.currentTimeMillis();
-        Log.d("TRULALA", "Время работы цикла: " + ((endTime - startTime) / 1000f));
+        //endTime = System.currentTimeMillis();
+        //Log.d("TRULALA", "Время работы цикла: " + ((endTime - startTime) / 1000f));
         return false;
     }
 
-    void preparingBeforeFindContours(Bitmap image, int numberOfFrame, String fileOfName) {
-        if (image != null) {
-            Igray = IplImage.create(image.getWidth(), image.getHeight(), IPL_DEPTH_8U, 1);
-            Icolor = IplImage.create(image.getWidth(), image.getHeight(), IPL_DEPTH_8U, 4);
-            image.copyPixelsToBuffer(Icolor.getByteBuffer());
-            cvCvtColor(Icolor, Igray, CV_RGB2GRAY);
-            double areaImage = (Igray.height() * Igray.width()) / MINAREA;
-
-            double offset = DOFFSET;
-
-            //БИНАРИЗАЦИЯ
-            Iat = cvCreateImage(cvGetSize(Igray), IPL_DEPTH_8U, 1);
-            cvThreshold(Igray, Iat, offset, 255, CV_THRESH_BINARY);
-
-            //контуры
-            if (findCountoursDetection(areaImage, Iat)) {
-                saveBitmapToPhone(image, fileOfName, numberOfFrame);
-            }
-            ;
+    boolean preparingBeforeFindContours(Bitmap image, int numberOfFrame, String fileOfName) {
+        if (image == null) {
+            return false;
         }
 
+        Igray = IplImage.create(image.getWidth(), image.getHeight(), IPL_DEPTH_8U, 1);
+        Icolor = IplImage.create(image.getWidth(), image.getHeight(), IPL_DEPTH_8U, 4);
+        image.copyPixelsToBuffer(Icolor.getByteBuffer());
+        cvCvtColor(Icolor, Igray, CV_RGB2GRAY);
+        double areaImage = (Igray.height() * Igray.width()) / MINAREA;
+
+        double offset = DOFFSET;
+
+        //БИНАРИЗАЦИЯ
+        Iat = cvCreateImage(cvGetSize(Igray), IPL_DEPTH_8U, 1);
+        cvThreshold(Igray, Iat, offset, 255, CV_THRESH_BINARY);
+
+        //контуры
+        if (findCountoursDetection(areaImage, Iat)) {
+            saveBitmapToPhone(image, fileOfName, numberOfFrame);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     void saveBitmapToPhone(Bitmap image, String fileOfName, int counter) {
-        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"Movies";
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Movies";
         File dir = new File(file_path);
         if (!dir.exists())
             dir.mkdirs();
-        File file = new File(dir, fileOfName+counter+".jpg");
+        File file = new File(dir, fileOfName + counter + ".jpg");
         FileOutputStream fOut = null;
         try {
             fOut = new FileOutputStream(file);
