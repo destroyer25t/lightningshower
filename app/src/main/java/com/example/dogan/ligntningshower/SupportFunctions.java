ac3d0635d92ead9d796542d80c2ec17002131784
Oleg <js7ussr@gmail.com>
2016-04-12 02:44:41 +0500

    -Добавлена функция определяющая количество физических ядер

--- app/src/main/java/com/example/dogan/ligntningshower/SupportFunctions.java
+++ app/src/main/java/com/example/dogan/ligntningshower/SupportFunctions.java
@@ -11,6 +11,8 @@ import android.provider.MediaStore;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.regex.Pattern;
 
 /**
  * Created by dogan on 03.04.2016.
@@ -182,5 +184,37 @@ public class SupportFunctions {
         File videoFile = new File(videoPathToDelete);
         videoFile.delete();
 
+    }
+
+    /**
+     * Gets the number of cores available in this device, across all processors.
+     * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+     *
+     * @return The number of cores, or 1 if failed to get result
+     */
+    private static int getNumCores() {
+        //Private Class to display only CPU devices in the directory listing
+        class CpuFilter implements FileFilter {
+            @Override
+            public boolean accept(File pathname) {
+                //Check if filename is "cpu", followed by a single digit number
+                if (Pattern.matches("cpu[0-9]+", pathname.getName())) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        try {
+            //Get directory containing CPU info
+            File dir = new File("/sys/devices/system/cpu/");
+            //Filter to only list the devices we care about
+            File[] files = dir.listFiles(new CpuFilter());
+            //Return the number of cores (virtual CPU devices)
+            return files.length;
+        } catch (Exception e) {
+            //Default to return 1 core
+            return 1;
+        }
     }
 }
