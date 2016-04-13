4ba22f90f7d24e3214728fc2dd2281ed9d262573
Oleg <js7ussr@gmail.com>
2016-04-14 03:53:49 +0500

    -Исправлено появление диалогового окна при нажатии Назад, если видео уже обработано

--- app/src/main/java/com/example/dogan/ligntningshower/ProcessingActivity.java
+++ app/src/main/java/com/example/dogan/ligntningshower/ProcessingActivity.java
@@ -42,16 +42,11 @@ import static org.bytedeco.javacv.Parallel.getNumCores;
 
 public class ProcessingActivity extends AppCompatActivity {
 
-    MediaMetadataRetriever_Decomposing_Task MediaMRetrTask;
-    JavaCVDecomposing_Task JavaCVTask;
-
     int typeOfTask;     //переменная в которой хранится выбор типа обработки
     Uri imageUri;       //путь URI до видео
     String videopath;   //путь до видео
     SharedPreferences prefs;    //настройки приложения
     float precision;
-
-    private static final int NOTIFY_ID = 13395;   //id нашего уведомления о выполнении поиска
 
     //чтобы была возможность убивать потоки и следить за состоянием пришлось вынести в глобал
 
@@ -68,7 +63,6 @@ public class ProcessingActivity extends AppCompatActivity {
     private volatile int secondsCounterThrVer = 0;
     private volatile int lightningsCounterThrVer = 0;
     private volatile int secondsInMinuteCounterTheVer = 0;
-
 
 
     //все необходимые для работы свойства видео
@@ -145,233 +139,6 @@ public class ProcessingActivity extends AppCompatActivity {
             controlThread.start();
         }
 
-    }
-
-    /**
-     * AsyncTask класс, реализующий обработку видео с помощью FFMPeg grabber-а
-     * Не рекомендуется использовать поскольку андроид гасит выполнение цикла в doInBackground
-     */
-    protected class JavaCVDecomposing_Task extends AsyncTask<String, Integer, Void> {
-        //получаем доступ к элементам интерфейса
-        ProgressBar horizontalprogress = (ProgressBar) findViewById(R.id.progressBarFrames);
-        TextView textviewCountSeconds = (TextView) findViewById(R.id.textViewCountSeconds);
-        TextView textViewCountLightnings = (TextView) findViewById(R.id.textViewFoundedLightnings);
-        TextView textViewCountFrames = (TextView) findViewById(R.id.textViewCountFrames);
-        ImageView imageView = (ImageView) findViewById(R.id.imageViewCurrentFrame);
-
-        int counterFrames = 0;        //счетчик кадров
-        int counterSeconds = 0;       //счетчик обработанных секунд
-        int counterLightnings = 0;     //счетчик молний
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            horizontalprogress.setMax(durationMs / frameRate);
-            horizontalprogress.setProgress(0);
-
-            Toast.makeText(getApplicationContext(), "Обработка видео запущена", Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... progress) {
-            super.onProgressUpdate(progress);
-            horizontalprogress.setProgress(progress[0]);
-            textviewCountSeconds.setText("Секунд видео обработано:" + String.valueOf(progress[1]) + " из " + String.valueOf(durationS));
-            textViewCountLightnings.setText("Молний обнаружено:" + String.valueOf(progress[2]));
-            textViewCountFrames.setText("Кадров обработано:" + String.valueOf(progress[0]) + " из " + String.valueOf(frames));
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            Bitmap bitmapVideoFrame;
-            Frame videoframe = null;
-            int currentFrame = 0;
-            String videofileName = getFileName(params[0]);
-            OpenCVHandler openCVHandler = new OpenCVHandler();
-
-            FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(params[0]);
-            AndroidFrameConverter converterToBitmap = new AndroidFrameConverter();
-
-            try {
-                grabber.start();
-            } catch (FrameGrabber.Exception e) {
-                e.printStackTrace();
-            }
-
-            while (true) {
-                if (isCancelled()) return null;
-                long startTime = System.currentTimeMillis();    //засекаем время получения кадра
-
-                try {
-                    videoframe = grabber.grab();
-                    currentFrame = grabber.getFrameNumber();
-                } catch (FrameGrabber.Exception e) {
-                    e.printStackTrace();
-                }
-
-
-                Log.d("Lightning Shower Debug:", "Кадр видео: " + currentFrame);
-                counterFrames++;
-                if (counterFrames == frameRate) {   //каждые FPS кадров сбрасываем счетчик кадров и добавляем секунду
-                    counterFrames = 0;
-                    counterSeconds++;
-                    Log.d("Lightning Shower Debug:", "Секунда видео: " + counterSeconds);
-                }
-
-
-                bitmapVideoFrame = converterToBitmap.convert(videoframe);
-                final Bitmap finalBitmapVideoFrame = bitmapVideoFrame;
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        imageView.setImageBitmap(finalBitmapVideoFrame);
-
-                    }
-                });
-                long endTime = System.currentTimeMillis();
-                Log.d("Lightning Shower Debug:", "Время выдергивания из видоса OPENCV: " + ((endTime - startTime) / 1000f));
-                publishProgress(currentFrame, counterSeconds, counterLightnings);
-                if (openCVHandler.preparingBeforeFindContours(bitmapVideoFrame, currentFrame, videofileName, precision)) {
-                    counterLightnings++;
-                }
-
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            horizontalprogress.setProgress(0);
-            Toast.makeText(getApplicationContext(), "Обработка видео окончена", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    /**
-     * AsyncTask класс, реализующий обработку видео с помощью гугловского MediaMetadataRetriever
-     * Не рекомендуется использовать поскольку андроид гасит выполнение цикла в doInBackground
-     */
-    protected class MediaMetadataRetriever_Decomposing_Task extends AsyncTask<String, Integer, Void> {
-        ProgressBar horizontalprogress = (ProgressBar) findViewById(R.id.progressBarFrames);
-        TextView textviewCountSeconds = (TextView) findViewById(R.id.textViewCountSeconds);
-        TextView textViewCountLightnings = (TextView) findViewById(R.id.textViewFoundedLightnings);
-        TextView textViewCountFrames = (TextView) findViewById(R.id.textViewCountFrames);
-        ImageView imageView = (ImageView) findViewById(R.id.imageViewCurrentFrame);
-
-        private int frameStep;//специальная переменная для работы getFrameAtTime
-
-        int counterFrames = 0;        //счетчик кадров
-        int counterSeconds = 0;       //счетчик обработанных секунд
-        int counterLightnings = 0;     //счетчик молний
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            frameStep = (int) (1000000 / frameRateDouble); //1000000/FPS - через каждые frameStep микросекунд следует брать новый кадр
-
-            horizontalprogress.setMax(durationMs / frameRate);
-            horizontalprogress.setProgress(0);
-
-            Toast.makeText(getApplicationContext(), "Обработка видео запущена", Toast.LENGTH_LONG).show();
-
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... progress) {
-            super.onProgressUpdate(progress);
-            horizontalprogress.setProgress(progress[0] / frameStep);
-            textviewCountSeconds.setText("Секунд видео обработано:" + String.valueOf(progress[1]));
-            textViewCountLightnings.setText("Кадров с молниями найдено:" + String.valueOf(progress[2]));
-            textViewCountFrames.setText("Кадров обработано:" + String.valueOf(progress[0] / frameStep));
-        }
-
-
-        @Override
-        protected Void doInBackground(String... params) {
-
-            MediaMetadataRetriever mediaMetadata = new MediaMetadataRetriever();
-            OpenCVHandler openCVHandler = new OpenCVHandler();
-
-            String videofileName = getFileName(params[0]);
-            Bitmap frame;
-
-            //устанавливаем источник для mediadata
-            mediaMetadata.setDataSource(params[0]);
-
-
-            for (int currentFrame = frameStep; currentFrame < durationMs * 1000; currentFrame += frameStep) {
-                if (isCancelled()) return null;
-                counterFrames++;
-                Log.d("Lightning Shower Debug:", "Кадр видео: " + currentFrame / frameStep);
-                if (counterFrames == frameRate) {   //каждые FPS кадров сбрасываем счетчик кадров и добавляем секунду
-                    counterFrames = 0;
-                    counterSeconds++;
-                    Log.d("Lightning Shower Debug:", "Секунда видео: " + counterSeconds);
-                }
-
-                long startTime = System.currentTimeMillis();    //засекаем время получения кадра
-                frame = mediaMetadata.getFrameAtTime(currentFrame, MediaMetadataRetriever.OPTION_CLOSEST);
-                long endTime = System.currentTimeMillis();
-
-                final Bitmap finalBitmapVideoFrame = frame;
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        imageView.setImageBitmap(finalBitmapVideoFrame);
-
-                    }
-                });
-                Log.d("Lightning Shower Debug:", "Время выдергивания из видоса: " + ((endTime - startTime) / 1000f));
-                if (openCVHandler.preparingBeforeFindContours(frame, currentFrame, videofileName, precision)) {
-                    counterLightnings++;
-                }
-                publishProgress(currentFrame, counterSeconds, counterLightnings);
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            horizontalprogress.setProgress(0);
-            Toast.makeText(getApplicationContext(), "Обработка видео окончена", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-
-    /**
-     * Функция позволяющая обновлять элементы интерфейса. Также в нее передается Bitmap
-     *
-     * @param img для обновления ImageView - эскиз текущего кадра
-     */
-    public void refreshUIFunction(final Bitmap img) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ProgressBar horizontalprogress = (ProgressBar) findViewById(R.id.progressBarFrames);
-                TextView textviewCountSeconds = (TextView) findViewById(R.id.textViewCountSeconds);
-                TextView textViewCountLightnings = (TextView) findViewById(R.id.textViewFoundedLightnings);
-                TextView textViewCountFrames = (TextView) findViewById(R.id.textViewCountFrames);
-                ImageView imageView = (ImageView) findViewById(R.id.imageViewCurrentFrame);
-
-                horizontalprogress.setProgress(framesCounterThrVer);
-                textviewCountSeconds.setText("Секунд видео обработано:" + String.valueOf(secondsCounterThrVer) + " из " + String.valueOf(durationS));
-                textViewCountLightnings.setText("Молний обнаружено:" + String.valueOf(lightningsCounterThrVer));
-                textViewCountFrames.setText("Кадров обработано:" + String.valueOf(framesCounterThrVer) + " из " + String.valueOf(frames));
-                imageView.setImageBitmap(img);
-            }
-        });
-
-    }
-
-    public void refreshUIAfterAll() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Button buttonStop = (Button) findViewById(R.id.buttonStop);
-                buttonStop.setVisibility(View.INVISIBLE);
-            }
-        });
     }
 
     /**
@@ -457,6 +224,7 @@ public class ProcessingActivity extends AppCompatActivity {
 
 
     }
+
     /**
      * Класс-поток, реализует обработку кадров переданных в конструктор с помощью FFmpeg
      * <p/>
@@ -578,10 +346,10 @@ public class ProcessingActivity extends AppCompatActivity {
             int counterFrames = 0;
             for (currentFrame = startFrame; currentFrame < endFrame; currentFrame += frameStep) {
 
-                counterFrames++;
+                secondsInMinuteCounterTheVer++;
                 Log.d("Lightning Shower Debug:", "Кадр видео: " + currentFrame / frameStep);
-                if (counterFrames == frameRate) {   //каждые FPS кадров сбрасываем счетчик кадров и добавляем секунду
-                    counterFrames = 0;
+                if (secondsInMinuteCounterTheVer == frameRate) {   //каждые FPS кадров сбрасываем счетчик кадров и добавляем секунду
+                    secondsInMinuteCounterTheVer = 0;
                     secondsCounterThrVer++;
                     Log.d("Lightning Shower Debug:", "Секунда видео: " + secondsCounterThrVer);
                 }
@@ -604,33 +372,22 @@ public class ProcessingActivity extends AppCompatActivity {
 
     }
 
+    //------------------------------------------------------------------------------------------
     //Обработчики кнопок
-    /*
+
+
     public void onClickStopButton(View view) {
-        if (typeOfTask == 0) {
-            JavaCVTask.cancel(true);
-            Button buttonStop = (Button) findViewById(R.id.buttonStop);
-            buttonStop.setVisibility(View.INVISIBLE);
-        } else {
-            MediaMRetrTask.cancel(true);
-        }
-    }
-    */
-    //Обработчики кнопок
-    public void onClickStopButton(View view) {
-        if (typeOfTask == 0) {
-            JavaCVTask.cancel(true);
-            Button buttonStop = (Button) findViewById(R.id.buttonStop);
-            buttonStop.setVisibility(View.INVISIBLE);
-        } else {
-            MediaMRetrTask.cancel(true);
-        }
+        killAllThreads();
     }
 
     @Override
     public void onBackPressed() {
-        // super.onBackPressed();
-        openQuitDialog();
+        if (numberOfExecutedThreads == getNumCores()) {
+            super.onBackPressed();
+        } else {
+            openQuitDialog();
+        }
+
     }
 
     private void killAllThreads() {
@@ -644,7 +401,7 @@ public class ProcessingActivity extends AppCompatActivity {
             }
         }
         if (lastThread.isAlive()) killThread(lastThread);
-
+        refreshUIAfterAll();
 
     }
 
@@ -669,5 +426,40 @@ public class ProcessingActivity extends AppCompatActivity {
         });
 
         quitDialog.show();
+    }
+
+    /**
+     * Функция позволяющая обновлять элементы интерфейса. Также в нее передается Bitmap
+     *
+     * @param img для обновления ImageView - эскиз текущего кадра
+     */
+    public void refreshUIFunction(final Bitmap img) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ProgressBar horizontalprogress = (ProgressBar) findViewById(R.id.progressBarFrames);
+                TextView textviewCountSeconds = (TextView) findViewById(R.id.textViewCountSeconds);
+                TextView textViewCountLightnings = (TextView) findViewById(R.id.textViewFoundedLightnings);
+                TextView textViewCountFrames = (TextView) findViewById(R.id.textViewCountFrames);
+                ImageView imageView = (ImageView) findViewById(R.id.imageViewCurrentFrame);
+
+                horizontalprogress.setProgress(framesCounterThrVer);
+                textviewCountSeconds.setText("Секунд видео обработано:" + String.valueOf(secondsCounterThrVer) + " из " + String.valueOf(durationS));
+                textViewCountLightnings.setText("Молний обнаружено:" + String.valueOf(lightningsCounterThrVer));
+                textViewCountFrames.setText("Кадров обработано:" + String.valueOf(framesCounterThrVer) + " из " + String.valueOf(frames));
+                imageView.setImageBitmap(img);
+            }
+        });
+
+    }
+
+    public void refreshUIAfterAll() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Button buttonStop = (Button) findViewById(R.id.buttonStop);
+                buttonStop.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
