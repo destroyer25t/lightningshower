8d4098748588a09cbe7fb3b362948ed833d170f0
Oleg <js7ussr@gmail.com>
2016-04-20 00:20:58 +0500

    -Дизайн начального активити

--- app/src/main/java/com/example/dogan/ligntningshower/MainActivity.java
+++ app/src/main/java/com/example/dogan/ligntningshower/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView mImageView = (ImageView) findViewById(R.id.testImageView);
     }
 
     //Активация и показ меню
